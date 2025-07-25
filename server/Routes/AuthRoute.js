5430c80ad8ef56f85701be61b6c8b7331f10ebae
ManishAditiya <work.manishaditiya@gmail.com>
2025-07-25 22:53:46 +0530

    client added

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,6 +1,10 @@
-const { Signup } = require("../Controllers/AuthController");
-const router = require("express").Router();
+const { Signup, Login } = require('../Controllers/AuthController')
+const { userVerification } = require('../Middlewares/AuthMiddleware');
 
-router.post("/signup", Signup);
+const router = require('express').Router()
 
-module.exports = router;
+router.post('/signup', Signup)
+router.post('/login', Login)
+router.post('/', userVerification)
+
+module.exports = router
